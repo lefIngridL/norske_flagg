a08b6faa3b412b5509ab95f68e39eab429032ea3
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-09-27 12:18:55 +0200

    innit

--- script2.js
+++ script2.js
@@ -13,7 +13,7 @@ function updateView(){
 
 // Controler
 flagg();
-function flagg(){
+function rWbWr(){
     let j= 0;
     
     while(j<6){
@@ -38,31 +38,42 @@ let i = 0;
 
     }  j++; 
     }
-    //j= 11;
-    while(j<16 && j>10){
-    let i = 0;
-    for(i;i<6;i++){
-        document.getElementById("flagg").innerHTML += `<div class="red">${j}</div>`;
-    }
-    for(i;i<7; i++){
-        document.getElementById("flagg").innerHTML += `<div>${j}</div>`;
-
-    }
-    for(i; i<9; i++){
-        document.getElementById("flagg").innerHTML += `<div class="blue">${j}</div>`;
-
-    }
-    for(i; i<10; i++){
-        document.getElementById("flagg").innerHTML += `<div>${j}</div>`;
-
-    }
-    for(i; i<22; i++){
-        document.getElementById("flagg").innerHTML += `<div class="red">${j}</div>`;
-
-    }  j++; 
-    }
 }
 
+function wBw(){
+    let j=0;
+    while(j<1){
+let i = 0;
+   for(i;i<7;i++){
+    document.getElementById("flagg").innerHTML += `<div>${j}</div>`;
+    }
+   for(i;i<9;i++){
+    document.getElementById("flagg").innerHTML += `<div class="blue">${j}</div>`;
+   }
+   for(i;i<22;i++){
+    document.getElementById("flagg").innerHTML += `<div>${j}</div>`;
+   } j++;
+     }  
+}
+   
+function blue(){
+    let j=0;
+    while(j<2){
+      for(let i=0;i<22;i++){
+        document.getElementById("flagg").innerHTML += `<div class="blue">${j}</div>`;
+
+    }  j++;
+    }
+    
+}
+
+function flagg(){
+    rWbWr();
+    wBw();
+    blue();
+    wBw();
+    rWbWr();
+}
 //rød-hvit-blå-hvit-rød
 //hvit-blå-hvit
 //blå
